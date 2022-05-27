3af43a660392d60a443d2c8e4d8484754d1abcb7
Jack Allen <jackallensemail+git@gmail.com>
2022-05-27 10:08:01 +0100

    modal

--- apps/push/src/app/app.controller.ts
+++ apps/push/src/app/app.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import axios from 'axios';
 
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Post("notification")
+  async notification(@Body() message: any) {
+    const { data } = await axios.post('https://exp.host/--/api/v2/push/send', message, {
+      headers: {
+
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      }
+    })
+
+    return data;
   }
 }
